c065947f0b93126248dde790f41236425b75d4c4
hueonju <fhseh14@gmail.com>
2026-07-29 15:44:51 +0900

    feat: 예외처리 추가, 스냅 창 전환 추가

---
+++ src/gesture/youtube_control.py
@@ -0,0 +1,8 @@
+class YoutubeController:
+    # 제스처 -> 유튜브 화면 제어(재생/일시정지, 볼륨 등) 매핑
+
+    def __init__(self):
+        pass
+
+    def handle(self, gesture_name: str):
+        pass
---
+++ src/smoke/__init__.py
@@ -0,0 +1,3 @@
+from .detector import SmokeDetector
+
+__all__ = ["SmokeDetector"]
